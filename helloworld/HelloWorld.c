832474b4c11415adb085e52784ffa2f002ba155e
mudongliang <mudongliangabcd@gmail.com>
2015-10-06 13:44:09 +0800

    add module info : author, description, version

--- helloworld/HelloWorld.c
+++ helloworld/HelloWorld.c
@@ -2,6 +2,9 @@
 #include <linux/init.h>
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("mudongliangabcd@gmail.com");
+MODULE_DESCRIPTION("HelloWorld Module Template");
+MODULE_VERSION("0.1");
 
 static int __init moduletest_init(void)
 {
